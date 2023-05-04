a75d0fbca2ecf03281366276176602125726d606
Hugo Morales <moralesbatzh@gmail.com>
2023-05-04 15:07:33 -0600

    aqui hice el ejercicio 2

--- ejemplo1.php
+++ ejemplo1.php
@@ -1,42 +1,43 @@
 <?php
-class Producto {
-    #dificinion de los atributos
-    public string $nombre;
-    public int $precio;
-    public bool $disponible;
-     #metodo constructor, se ejecuta al efectuar la instancia
-     public function __construct(string $nombre, int $precio, bool $disponible)
-     {
-        #se les da valores a los atributos con los parametros que recibe el metodo constructor 
-        $this-> nombre= $nombre;
-        $this-> precio= $precio;
-        $this-> disponible= $disponible;
 
-     }
-     #definicion de metodos 
-     public function mostrarProducto(){
-        echo "El Producto es: ".$this->nombre. " y su precio es de: ".$this->precio;
-     }
+class Producto {
+   #dificinion de los atributos
+   public string $nombre;
+   public int $precio;
+   public bool $disponible;
+    #metodo constructor, se ejecuta al efectuar la instancia
+    public function __construct(string $nombre, int $precio, bool $disponible)
+    {
+       #se les da valores a los atributos con los parametros que recibe el metodo constructor 
+       $this-> nombre= $nombre;
+       $this-> precio= $precio;
+       $this-> disponible= $disponible;
+
+    }
+    #definicion de metodos 
+    public function mostrarProducto(){
+       echo "El Producto es: ".$this->nombre. " y su precio es de: ".$this->precio;
+    }
 
 }
 //instancia de la clase en el primer objeto
 $producto= new Producto('Tablet',200, true);
 //llamado al metodo mostrar productom utilizando sintaxis de flecha ->
 $producto -> mostrarProducto();
- //impresion en pantalla del primer objeto 
- echo "<pre>";
- var_dump($producto);
- echo "</pre>";
+//impresion en pantalla del primer objeto 
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
 
- //instancia de la clase en el segundo objeto 
- $producto2 = new Producto('Monitor Curvo', 300, true);
- //llamando al metodo mostrar producto, utilizando sintaxis de la flecha ->
- $producto2 -> mostrarProducto();
+//instancia de la clase en el segundo objeto 
+$producto2 = new Producto('Monitor Curvo', 300, true);
+//llamando al metodo mostrar producto, utilizando sintaxis de la flecha ->
+$producto2 -> mostrarProducto();
 
- //impresion en pantlla del segundo objeto
+//impresion en pantlla del segundo objeto
 
- echo "<pre>";
- var_dump($producto2);
- echo "</pre>";
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
 
 ?>
