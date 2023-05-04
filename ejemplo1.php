268ece1b98b74ea7b31e7d5e151b6335784ebf20
Hugo Morales <moralesbatzh@gmail.com>
2023-05-04 10:44:51 -0600

    corregi unos errores que tenia

--- ejemplo1.php
+++ ejemplo1.php
@@ -5,7 +5,8 @@ class Producto {
     public int $precio;
     public bool $disponible;
      #metodo constructor, se ejecuta al efectuar la instancia
-     public function_construct(string $nombre, int $precio, bool $disponible){
+     public function __construct(string $nombre, int $precio, bool $disponible)
+     {
         #se les da valores a los atributos con los parametros que recibe el metodo constructor 
         $this-> nombre= $nombre;
         $this-> precio= $precio;
@@ -20,6 +21,22 @@ class Producto {
 }
 //instancia de la clase en el primer objeto
 $producto= new Producto('Tablet',200, true);
-//llamado al metodo mostrar productom utilizando sintaxis de flecha 
+//llamado al metodo mostrar productom utilizando sintaxis de flecha ->
+$producto -> mostrarProducto();
+ //impresion en pantalla del primer objeto 
+ echo "<pre>";
+ var_dump($producto);
+ echo "</pre>";
+
+ //instancia de la clase en el segundo objeto 
+ $producto2 = new Producto('Monitor Curvo', 300, true);
+ //llamando al metodo mostrar producto, utilizando sintaxis de la flecha ->
+ $producto2 -> mostrarProducto();
+
+ //impresion en pantlla del segundo objeto
+
+ echo "<pre>";
+ var_dump($producto2);
+ echo "</pre>";
 
 ?>
